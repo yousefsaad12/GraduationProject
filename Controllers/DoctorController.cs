a0858506b4f012023357b1deed24600ef472152f
Yousef Saad <saadrslan95@gmail.com>
2025-05-02 17:22:59 +0300

    done

---
+++ Controllers/DoctorController.cs
@@ -0,0 +1,68 @@
+
+using GraduationProject.InterFaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace GraduationProject.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DoctorController : ControllerBase
+    {
+        private readonly IDoctorInterface _doctorInterface;
+
+        public DoctorController(IDoctorInterface doctorInterface)
+        {
+            _doctorInterface = doctorInterface;
+        }
+
+        [HttpGet]
+        [Route("GetAllDoctor")]
+        public async Task<IActionResult> GetAllDoctor()
+        {
+            IReadOnlyList<DoctorResponse> doctorResponses = await _doctorInterface.GetAllDoctors().ConfigureAwait(false);
+
+            return Ok(doctorResponses);
+        }
+
+        [HttpPost]
+        [Route("AddDoctor")]
+        public async Task<IActionResult> AddDoctor([FromBody] CreateDoctorRequest createDoctorRequest)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            ServicesResult<bool> result = await _doctorInterface.AddDoctor(createDoctorRequest).ConfigureAwait(false);
+
+            if (result.Success) return Created(string.Empty, "Doctor successfully added.");
+
+            return BadRequest(result);
+
+        }
+
+        [HttpDelete]
+        [Route("DeleteDoctor")]
+        public async Task<IActionResult> AddDoctor([FromQuery] int id)
+        {
+
+            ServicesResult<bool> result = await _doctorInterface.DeleteDoctor(id).ConfigureAwait(false);
+
+            if (result.Success) return NoContent();
+
+            return NotFound("Doctor not found");
+
+        }
+
+        [HttpPut]
+        [Route("UpdateDoctor")]
+        public async Task<IActionResult> UpdateDoctor([FromQuery] int id, CreateDoctorRequest updateDoctorRequest)
+        {
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+
+            ServicesResult<bool> result = await _doctorInterface.UpdateDoctor(updateDoctorRequest, id).ConfigureAwait(false);
+
+            if (result.Success) return Ok("Doctor successfully updated");
+
+            return NotFound("Doctor not found.");
+        }
+    }
+}
